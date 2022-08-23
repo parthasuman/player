dc2d102ffff8a60d11cc4e52ccc44e067c15c7ec
parthasuman <parthasuman123@gmail.com>
2022-08-23 12:30:45 +0600

    another js file clen

--- js/player.js
+++ js/player.js
@@ -1,37 +1,18 @@
-// function getPlayerName(inputId) {
-//   const playerName = document.getElementById(inputId);
-//   const playerFirst = playerName.innerText;
-//   const select = document.getElementById("list");
-//   const li = document.createElement("li");
-//   li.innerText = playerFirst;
-//   select.appendChild(li);
-// }
-
 document.getElementById("player1").addEventListener("click", function () {
-  // getPlayerName("player-jesus");
   document.getElementById("player1").disabled = true;
 });
 document.getElementById("player2").addEventListener("click", function () {
-  // getPlayerName("player-Martinelli");
   document.getElementById("player2").disabled = true;
 });
 document.getElementById("player3").addEventListener("click", function () {
-  // getPlayerName("player-White");
   document.getElementById("player3").disabled = true;
 });
 document.getElementById("player4").addEventListener("click", function () {
-  // getPlayerName("player-saka");
   document.getElementById("player4").disabled = true;
 });
 document.getElementById("player5").addEventListener("click", function () {
-  // getPlayerName("player-smith");
   document.getElementById("player5").disabled = true;
 });
 document.getElementById("player6").addEventListener("click", function () {
-  // getPlayerName("player-martin");
-
   document.getElementById("player6").disabled = true;
 });
-
-// const totalLi = document.getElementById("list");
-// console.log(totalLi);
