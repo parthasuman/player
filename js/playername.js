9363ee6e46f8a6135737799928bf57173a15ba0a
parthasuman <parthasuman123@gmail.com>
2022-08-23 12:28:39 +0600

    js file clear

--- js/playername.js
+++ js/playername.js
@@ -3,7 +3,7 @@ const playerArray = [];
 function display(listPlayer) {
   console.log(listPlayer);
   const unoderList = document.getElementById("list");
-  // console.log(unoderList);
+
   unoderList.innerHTML = "";
   document.getElementsByClassName("mm").disabled = true;
   for (let i = 0; i < listPlayer.length; i++) {
@@ -11,9 +11,9 @@ function display(listPlayer) {
       alert("not possible");
     } else {
       const name = playerArray[i].playerName;
-      // console.log(name);
+
       const ul = document.createElement("ul");
-      // console.log(ul);
+
       ul.innerHTML = `
       <li>${name}</li>
       `;
